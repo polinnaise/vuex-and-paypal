d53257b2bb61c19930f2b5f52a1e6520245df574
stranger <stranger@strangers-MacBook-Pro.local>
2020-07-07 19:01:48 +0200

    first

---
+++ src/components/navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h3>DORA GOLDSMITH</h3>
+    <nav>
+      <router-link v-for="routes in links" 
+      v-bind:key="routes.id"
+      :to="`${routes.page}`"> {{routes.text}} </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'Home',
+          page:'/'
+        },
+        {
+          id: 1,
+          text: 'Bio',
+          page:'/bio'
+        },
+        {
+          id: 2,
+          text: 'Mendesian',
+          page:'/exhibitions/mendesian'
+        },
+        {
+          id: 3,
+          text: 'Mummification',
+          page:'/exhibitions/mummification'
+        },
+        {
+          id: 4,
+          text: 'Conferences',
+          page:'/lectures/conferences'
+        },
+        {
+          id: 5,
+          text: 'Invited Lectures',
+          page:'/lectures/invited_lectures'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,71 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from "vue-router";
+import Home from "./components/home";
+import Bio from "./components/bio";
+import Mendesian from "./components/mendesian";
+import Mummification from "./components/mummification";
+import Conferences from "./components/conferences";
+import InvitedLectures from "./components/invited_lectures";
+
+Vue.use(Router);
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/bio/',
+      name: 'Bio',
+      component: Bio
+    },
+    {
+      path: '/exhibitions/mendesian',
+      name: 'Mendesian',
+      component: Mendesian
+    },
+    {
+      path: '/exhibitions/mummification',
+      name: 'Mummification',
+      component: Mummification
+    },
+    {
+      path: '/lectures/conferences',
+      name: 'Conferences',
+      component: Conferences
+    },
+    {path: '/lectures/invited_lectures',
+      name: 'Invited Lectures',
+      component: InvitedLectures
+    },
+    {
+      path: '/publications/',
+      name: 'Bio',
+      component: Bio
+    },
+    {
+      path: '/workshops',
+      name: 'Bio',
+      component: Bio
+    },
+    {
+      path: '/smellkits/',
+      name: 'Bio',
+      component: Bio
+    },
+    {
+      path: '/media/',
+      name: 'Bio',
+      component: Bio
+    }
+  ]
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
